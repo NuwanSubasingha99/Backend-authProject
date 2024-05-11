8367578c8b5b4bb8d99cfa2f62b2b4b3b9dfcef9
Nuwan Subhasingha <126569796+NuwanSubasingha99@users.noreply.github.com>
2024-05-12 00:42:55 +0530

    mvc

--- api/index.js
+++ api/index.js
@@ -1,19 +1,36 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import dotenv from 'dotenv';
 const app = express();
+dotenv.config();
 
-app.use('/api/login',(req,res)=>{
-    return res.send("Login Succesfull");
-})
+const uri = process.env.MONGO_URL
 
-app.use('/api/register',(req,res)=>{
-    return res.send("register Succesfull");
-})
+const client = new MongoClient(uri, {
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    // Connect the client to the server  (optional starting in v4.7)
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
 
 
-app.use('/',(req,res)=>{
-    return res.send("Hello welcome")
-})
+
+
+
 
 app.listen(8800,()=>{
+    run().catch(console.dir);
     console.log('Backend is running on port 8800');
 })
